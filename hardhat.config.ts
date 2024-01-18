688e85c979cfb5e1b39f2ea49e81173f3e180000
matt-wid3 <matthew@wid3.org>
2024-01-18 03:53:46 +0100

    added optimising sepholia configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter"
+
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
+if (!deployerPrivateKey) {
+  throw new Error("DEPLOYER_PRIVATE_KEY environment variable is not set.");
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   gasReporter: {
     enabled: false //Enable to get gas usage report when testing with `npx hardhat test`
+  },
+  networks: {
+    optimismSepolia: {
+      url: process.env.OPTIMISM_SEPOLIA_RPC_URL,
+      accounts: [deployerPrivateKey],
+    },
   }
 };
 
