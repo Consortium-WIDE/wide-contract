b98ef79bfd78187d58882ba969a449b98d29e4d8
matt-wid3 <matthew@wid3.org>
2024-02-05 18:50:58 +0100

    Updated contract to enable deployment on alastria

--- contracts/WideSignatureLogger.sol
+++ contracts/WideSignatureLogger.sol
@@ -3,7 +3,8 @@
 // Contract built by wid3.xyz for the wid3 platform.
 // Copyright (c) 2024
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract WideSignatureLogger {
     address public owner;
@@ -24,27 +25,21 @@ contract WideSignatureLogger {
     }
 
     mapping(bytes32 => PayloadInfo) public payloads;
-
     mapping(bytes32 => PresentationInfo[]) public presentations;
 
     event PayloadLogged(bytes32 indexed payloadKey, uint256 timestamp);
-    event PresentationLogged(
-        bytes32 indexed presentationKey,
-        uint256 timestamp
-    );
+    event PresentationLogged(bytes32 indexed presentationKey, uint256 timestamp);
+
+    constructor() public {
+        owner = msg.sender;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the contract owner can call this");
         _;
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function logPayload(
-        PayloadSignaturePair memory payloadPair
-    ) public onlyOwner {
+    function logPayload(PayloadSignaturePair memory payloadPair) public onlyOwner {
         bytes32 payloadKey = payloadPair.payloadKey;
         bytes memory signature = payloadPair.signature;
 
@@ -55,38 +50,26 @@ contract WideSignatureLogger {
         emit PayloadLogged(payloadKey, block.timestamp);
     }
 
-    function logPayloadBatch(
-        PayloadSignaturePair[] memory payloadPairs
-    ) public onlyOwner {
+    function logPayloadBatch(PayloadSignaturePair[] memory payloadPairs) public onlyOwner {
         for (uint i = 0; i < payloadPairs.length; i++) {
             logPayload(payloadPairs[i]);
         }
     }
 
-    function logPresentation(
-        bytes32 presentationKey,
-        string memory jsonString
-    ) public onlyOwner {
+    function logPresentation(bytes32 presentationKey, string memory jsonString) public onlyOwner {
         require(presentationKey != bytes32(0), "Invalid input");
 
-        PresentationInfo memory newPresentation = PresentationInfo(
-            jsonString,
-            block.timestamp
-        );
+        PresentationInfo memory newPresentation = PresentationInfo(jsonString, block.timestamp);
         presentations[presentationKey].push(newPresentation);
 
         emit PresentationLogged(presentationKey, block.timestamp);
     }
 
-    function getPayloadInfo(
-        bytes32 payloadKey
-    ) public view returns (PayloadInfo memory) {
+    function getPayloadInfo(bytes32 payloadKey) public view returns (PayloadInfo memory) {
         return payloads[payloadKey];
     }
 
-    function getPresentationHistory(
-        bytes32 presentationKey
-    ) public view returns (PresentationInfo[] memory) {
+    function getPresentationHistory(bytes32 presentationKey) public view returns (PresentationInfo[] memory) {
         return presentations[presentationKey];
     }
 }
